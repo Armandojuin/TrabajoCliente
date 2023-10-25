089422112843bd570741a40fe70481852558ad17
Javier Ortiz Moncayo <franciscojavier.ma40704@cesurformacion.com>
2023-10-25 09:38:16 +0200

    variables modificadas, cons => let

--- js/contact.js
+++ js/contact.js
@@ -2,9 +2,9 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     event.preventDefault();// evita que la pagina se recargue
 
     //Almacena cada valor que llega del formulario html en una variable
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
 
     //Como no tenemos backed vamos a mostrar lo enviado por la consola para combrobar que funciona
     console.log("Nombre: " + name);
